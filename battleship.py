0394ba8e74079cfd1f9029980e439486e62e102e
Darsh Kale <kale0085@umn.edu>
2022-04-26 15:20:42 -0500

    shtuf

--- battleship.py
+++ battleship.py
@@ -18,14 +18,22 @@ PrevIAShoots = []
 
 def main():
   strategyOne = []
-  for a in range(10000):
+  for a in range(1):
     clear_board(gridPlayerA) 
     clear_board(gridPlayerB)
     clear_board(gridGamePlayerA) 
     clear_board(gridGamePlayerB)
     PrevPlayerShoots = []
-    PrevIAShoots = []  
-    result = SingleMatch()
+    PrevIAShoots = [] 
+
+    
+    put_boats(gridPlayerA)#put IA's boats randomly
+    while sum(gridPlayerA) != 14:
+      clear_board(gridPlayerA) 
+      put_boats(gridPlayerA)
+
+    result = ConseqStrategy(gridPlayerA=gridPlayerA)
+
     #print(result)
     strategyOne.append(result)
 
@@ -88,8 +96,149 @@ def SingleMatch():
     #   print(gridGamePlayerA[i*10:i*10+10])
     return temp, BoatTouchedByPlayer, BoatTouchedByIA
        
-        
 
+def ConseqStrategy(gridPlayerA):
+    BoatTouchedByPlayer = 0      
+    PrevPlayerShoots = []
+    # clear_board(gridPlayerA)
+    # put_boats(gridPlayerA)#put IA's boats randomly
+
+    # while sum(gridPlayerA) != 14:
+    #   clear_board(gridPlayerA) 
+    #   put_boats(gridPlayerA)
+
+    temp = 0
+    while (BoatTouchedByPlayer < 14):
+        Player_Shoot = temp
+        if can_shoot(PrevPlayerShoots, Player_Shoot) == True: 
+            if gridPlayerA[Player_Shoot] == 1: 
+                shoot_Touched(gridGamePlayerA, Player_Shoot) # call the shoot function that replace the coord by an F on the empty board
+                BoatTouchedByPlayer = BoatTouchedByPlayer + 1 # count until 14 (number of boat "parts")
+            else:
+                shoot(gridGamePlayerA, Player_Shoot)
+        temp += 1
+        #print("temp, BoatTouched: ", temp, BoatTouchedByPlayer)
+    for i in range(10):
+      print(gridGamePlayerA[i*10:i*10+10])
+    return temp, BoatTouchedByPlayer
+
+# def RandomStrategy():
+#     BoatTouchedByIA = 0
+#     PrevIAShoots = []
+#     clear_board
+#     while (BoatTouchedByIA < 14):
+#         IA_shot = random.randint(0, 99) 
+#             while can_shoot(PrevIAShoots, IA_shot) == False:
+#                 IA_shot = random.randint(0, 99)
+
+
+#             if gridPlayerB[IA_shot] == 1:
+#                 #print("IA touched one of your boat")
+#                 shoot_Touched(gridGamePlayerB, IA_shot) 
+#                 #print(gridPlayerB)
+#                 BoatTouchedByIA = BoatTouchedByIA + 1 
+
+#             else:
+#                 shoot(gridGamePlayerB, IA_shot) # replace the shoot by an X
+#                 #print("Lucky, IA missed !")
+
+
+def HuntDestroy():
+    BoatTouchedByPlayer = 0
+    BoatTouchedByIA = 0
+      
+    PrevPlayerShoots = []
+    PrevIAShoots = [] 
+    clear_board(gridPlayerA)
+    put_boats(gridPlayerA)#put IA's boats randomly
+
+    total_shots = 0
+    while sum(gridPlayerA) != 14:
+      clear_board(gridPlayerA) 
+      put_boats(gridPlayerA)
+      #print(sum(gridPlayerA))
+
+    # deepcopy grid A to grid B (so boats orientations are that same on both grids)
+    gridPlayerB = copy.deepcopy(gridPlayerA) 
+    mode = "hunt"
+    # for i in range(10):
+    #   print(gridPlayerA[i*10:i*10+10]) # print the IA's grid (with no boats)
+    # print("SUM: ", sum(gridPlayerA))
+    firstHuntSquare = []
+    d_index = 0
+    dFirstShot = False
+    d_list = [-1, -10, 1, 10]
+    dOpposite = False
+    while (BoatTouchedByIA < 14) and (BoatTouchedByPlayer < 14):
+        # for i in range(10):
+        #   print(gridGamePlayerA[i*10:i*10+10]) 
+        Player_Shoot = random.randint(0, 99)
+        while can_shoot(PrevPlayerShoots, Player_Shoot) == False:
+            Player_Shoot = random.randint(0, 99)
+
+        if mode == "hunt":
+            if gridPlayerA[Player_Shoot] == 1:
+                #print("IA touched one of your boat")
+                shoot_Touched(gridGamePlayerA, Player_Shoot) 
+                firstHuntSquare.append(Player_Shoot)
+                dFirstShot = True
+                d_index = 0
+                #print(gridPlayerB)
+                BoatTouchedByPlayer = BoatTouchedByPlayer + 1
+
+                mode = "destroy"
+
+            else:
+                shoot(gridGamePlayerA, Player_Shoot)
+
+                ## IA STEP
+
+                #TODO: IA SHIT
+
+        if mode == "destroy":
+            if dFirstShot == True: # FIRST SHOT
+                current_square = firstHuntSquare[-1]
+                
+                Player_Shoot = d_list[d_index] + current_square
+                while can_shoot(PrevPlayerShoots, Player_Shoot) == False:
+                    d_index += 1
+                    Player_Shoot = d_list[d_index] + current_square
+                if gridPlayerA[Player_Shoot] == 1:
+                    shoot_Touched(gridGamePlayerA, Player_Shoot) 
+                    BoatTouchedByPlayer = BoatTouchedByPlayer + 1
+                else:
+                    shoot(gridGamePlayerA, Player_Shoot)
+
+                    ## IA STEP
+
+                    #TODO: IA SHIT
+
+
+
+            else: # Shots after the first
+                base_square = firstHuntSquare[-1]
+                current_square = PrevPlayerShoots[-1]
+                if dOpposite == False:
+                    Player_Shoot = current_square + d_list[d_index]
+
+                else:
+                    Player_Shoot = 6
+                    if can_shoot(PrevPlayerShoots, Player_Shoot) == False:
+                        a = 3
+                
+            
+
+            
+            
+
+
+            
+
+                
+
+    total_shots = 1
+
+    return total_shots, BoatTouchedByPlayer, BoatTouchedByIA
     
 def shoot_Touched(grid, shot):
     grid.pop(shot)
